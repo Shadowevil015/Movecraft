0f278df4e9445e1a90ac66f6d9542538aaf4e3e6
galacticwarrior9 <zareef1@yahoo.com>
2021-04-27 21:08:59 +0100

    Floating ground vehicle fix

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/async/translation/TranslationTask.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/async/translation/TranslationTask.java
@@ -827,7 +827,8 @@ public class TranslationTask extends AsyncTask {
                 hitGround = !testType.isAir() &&
                         !craft.getType().getPassthroughBlocks().contains(testType) &&
                         !(craft.getType().getHarvestBlocks().contains(testType) &&
-                        craft.getType().getHarvesterBladeBlocks().contains(ml.toBukkit(craft.getW()).getBlock().getType())) ||
+                        craft.getType().getHarvesterBladeBlocks().contains(ml.toBukkit(craft.getW()).getBlock().getType())) &&
+                        !hitBox.contains(dropped) ||
                         craft.getType().getMinHeightLimit(craft.getW()) == translated.translate(0, dropDistance + 1 , 0).getY();
 
                 if (hitGround) {
